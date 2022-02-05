4fc052bb71139b5ae42eb1457f4c3f728b0fdf51
DanCrk <dancrkyt@gmail.com>
2022-02-04 18:49:41 -0600

    Caso de uso para recuperar todos los contactos

---
+++ app/src/main/java/com/prueba/contactos/domain/GetAllContactsUseCase.kt
@@ -0,0 +1,13 @@
+package com.prueba.contactos.domain
+
+import com.prueba.contactos.Repositorio
+import com.prueba.contactos.data.model.Contacto
+import javax.inject.Inject
+
+class GetAllContactsUseCase @Inject constructor(private val repositorio: Repositorio){
+
+    // establecemos el caso de uso donde recuperamos todos los contactos
+    operator fun invoke():List<Contacto>{
+        return repositorio.getAllContacts()
+    }
+}
